b18af9d74af762ac2926f046f9298cc760ddf9c3
MinimumADHD <simone.cappe06@gmail.com>
2023-11-01 08:19:42 +0100

    all in a day's work

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,43 @@
 use std::io;
+use std::fs::File;
+use std::io::Write;
+use rand::Rng;
+
 
 fn get_input_from_keyboard(text_to_print: &str) -> String {
-    let mut _input_string = String::new();
+    let mut input_string = String::new();
     if !text_to_print.is_empty() && text_to_print.is_ascii() {
-        io::stdin().read_line(&mut _input_string).expect("FAILED TO READ LINE");
+        println!("{}", text_to_print);
+        io::stdin().read_line(&mut input_string).expect("FAILED TO READ LINE.");
     } else {
-        Err("ERROR: TEXT TO PRINT CAN'T BE EMPTY VALUE.")
+        panic!("ERROR: TEXT TO PRINT CAN'T BE EMPTY VALUE.");
     }
-    return _input_string;
+    input_string.trim().to_string()
 }
 
-fn gen_rand_al() {
+fn gen_rand_al() -> u8 {
+    let mut rng = rand::thread_rng();
     let _al = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+    let rand_num = rng.gen_range(0..=_al.len());
+    _al.as_bytes()[rand_num]
 }
 
-fn gen_rand_num() {
+fn gen_rand_num() -> u8 {
+    let mut rng = rand::thread_rng();
     let _num = "0123456789";
+    let rand_num = rng.gen_range(0..=_num.len());
+    _num.as_bytes()[rand_num]
 }
 
-fn get_rand_spec() {
+fn get_rand_spec() -> u8 {
+    let mut rng = rand::thread_rng();
     let _spec = "-_!=";
+    let rand_num = rng.gen_range(0..=_spec.len());
+    _spec.as_bytes()[rand_num]
 }
 
 fn main() {
     let _website_name = get_input_from_keyboard("INSERT THE SERVICE'S NAME.");
-    println!("{}", _website_name)
+    println!("You entered {}", _website_name);
 }
+
